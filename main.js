397aca9979e27915610715b25cfa37b70acfc0fd
Art <artnivnea@gmail.com>
2025-06-18 23:54:13 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,14 +1,23 @@
 import { decomposeTime } from "./modules/timeConvertor.js";
 import { displayMessages , clearText } from "./modules/dialogManager.js";
 
-const response = await fetch('./data/messages.json');
-const messages = await response.json();
-
 const countdown = document.getElementById("countdown");
 const artistImage = document.getElementById("artist-img");
 const horn = new Audio("./assets/horn.mp3");
 
 const birthday = new Date(2025, 5, 19);
+
+const messages = [
+  "Hei, salut!",
+  "Am auzit că astăzi e ziua ta.",
+  "Așadar...",
+  "La Mulți Ani!",
+  "Din nefericire, n-am știut ce cadou să-ți facem...",
+  "Așa că am decis să îți dăm 500 robux, ce zici?",
+  "Verifică-ți contul, ar trebui să fie ajuns deja.",
+  "Oh! Se face târziu, ar fi bine să plecăm.",
+  "Paa!"
+]
 
 const timer = setInterval(function() {
   const now = new Date();
